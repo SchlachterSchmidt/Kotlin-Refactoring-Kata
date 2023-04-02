efeef218f9702165550fcc970f6d227a5c4674be
philipp g <teenageenemanursesinbondage@gmail.com>
2023-04-02 17:41:29 +0200

    move update to method on BaseItem

--- Java/src/main/java/com/gildedrose/Item.kt
+++ Java/src/main/java/com/gildedrose/Item.kt
@@ -1,9 +1,61 @@
 package com.gildedrose
 
-data class Item(
+open class Item(
     val name: String,
     var sellIn: Int = 0,
     var quality: Int = 0
 ) {
     override fun toString() = "$name, $sellIn, $quality"
+}
+
+
+class BaseItem(name: String, sellIn: Int, quality: Int) : Item(name, sellIn, quality) {
+    fun update() {
+        if (name != "Aged Brie"
+            && name != "Backstage passes to a TAFKAL80ETC concert"
+        ) {
+            if (quality > 0) {
+                if (name != "Sulfuras, Hand of Ragnaros") {
+                    quality = quality - 1
+                }
+            }
+        } else {
+            if (quality < 50) {
+                quality = quality + 1
+                if (name == "Backstage passes to a TAFKAL80ETC concert") {
+                    if (sellIn < 11) {
+                        if (quality < 50) {
+                            quality = quality + 1
+                        }
+                    }
+                    if (sellIn < 6) {
+                        if (quality < 50) {
+                            quality = quality + 1
+                        }
+                    }
+                }
+            }
+        }
+        if (name != "Sulfuras, Hand of Ragnaros") {
+            sellIn = sellIn - 1
+        }
+        if (sellIn < 0) {
+            if (name != "Aged Brie") {
+                if (name != "Backstage passes to a TAFKAL80ETC concert") {
+                    if (quality > 0) {
+                        if (name != "Sulfuras, Hand of Ragnaros") {
+                            quality = quality - 1
+                        }
+                    }
+                } else {
+                    quality = quality - quality
+                }
+            } else {
+                if (quality < 50) {
+                    quality = quality + 1
+                }
+            }
+        }
+    }
+
 }
--- Java/src/test/java/com/gildedrose/GildedRoseTest.java
+++ Java/src/test/java/com/gildedrose/GildedRoseTest.java
@@ -8,7 +8,7 @@ class GildedRoseTest {
 
     @Test
     void foo() {
-        Item[] items = new Item[] { new Item("foo", 0, 0) };
+        Item[] items = new Item[] { new BaseItem("foo", 0, 0) };
         GildedRose app = new GildedRose(items);
         app.updateQuality();
         assertEquals("foo", app.getItems()[0].getName());
