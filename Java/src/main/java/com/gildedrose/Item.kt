5233846f8fe0eaa9f12a04f0df8dc85853fe823d
philipp g <teenageenemanursesinbondage@gmail.com>
2023-04-02 18:11:04 +0200

    introduced age and update3

--- Java/src/main/java/com/gildedrose/Item.kt
+++ Java/src/main/java/com/gildedrose/Item.kt
@@ -3,7 +3,7 @@ package com.gildedrose
 open class Item(
     val name: String,
     var sellIn: Int = 0,
-    var quality: Int = 0
+    var quality: Int = 0,
 ) {
     override fun toString() = "$name, $sellIn, $quality"
 }
@@ -12,26 +12,8 @@ open class Item(
 open class BaseItem(name: String, sellIn: Int, quality: Int) : Item(name, sellIn, quality) {
     fun update() {
         update1()
-        if (name != "Sulfuras, Hand of Ragnaros") {
-            sellIn = sellIn - 1
-        }
-        if (sellIn < 0) {
-            if (name != "Aged Brie") {
-                if (name != "Backstage passes to a TAFKAL80ETC concert") {
-                    if (quality > 0) {
-                        if (name != "Sulfuras, Hand of Ragnaros") {
-                            quality = quality - 1
-                        }
-                    }
-                } else {
-                    quality = quality - quality
-                }
-            } else {
-                if (quality < 50) {
-                    quality = quality + 1
-                }
-            }
-        }
+        age()
+        update3()
     }
 
     protected open fun update1() {
@@ -39,10 +21,25 @@ open class BaseItem(name: String, sellIn: Int, quality: Int) : Item(name, sellIn
             quality = quality - 1
         }
     }
+
+    protected open fun age() {
+        sellIn = sellIn - 1
+    }
+
+
+    protected open fun update3() {
+        if (sellIn < 0) {
+            if (quality > 0) {
+                quality -= 1
+            }
+        }
+    }
 }
 
 class Sulfuras(name: String, sellIn: Int, quality: Int) : BaseItem(name, sellIn, quality) {
     override fun update1() {}
+    override fun age() {}
+    override fun update3() {}
 }
 
 class Brie(name: String, sellIn: Int, quality: Int) : BaseItem(name, sellIn, quality) {
@@ -51,22 +48,37 @@ class Brie(name: String, sellIn: Int, quality: Int) : BaseItem(name, sellIn, qua
             quality += 1
         }
     }
+
+    override fun update3() {
+        if (sellIn < 0) {
+            if (quality < 50) {
+                quality += 1
+            }
+        }
+    }
+
 }
 
 class Pass(name: String, sellIn: Int, quality: Int) : BaseItem(name, sellIn, quality) {
     override fun update1() {
         if (quality < 50) {
-            quality = quality + 1
+            quality += 1
             if (sellIn < 11) {
                 if (quality < 50) {
-                    quality = quality + 1
+                    quality += 1
                 }
             }
             if (sellIn < 6) {
                 if (quality < 50) {
-                    quality = quality + 1
+                    quality += 1
                 }
             }
+        }
+    }
+
+    override fun update3() {
+        if (sellIn < 0) {
+            quality -= quality
         }
     }
 }
