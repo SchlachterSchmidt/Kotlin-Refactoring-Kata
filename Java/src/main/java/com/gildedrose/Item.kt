33b8fa41611cde4cae29496b022af8ba7c2af52a
philipp g <teenageenemanursesinbondage@gmail.com>
2023-04-02 22:12:17 +0200

    replace degrade with degradation and saturate with saturation

--- Java/src/main/java/com/gildedrose/Item.kt
+++ Java/src/main/java/com/gildedrose/Item.kt
@@ -11,49 +11,41 @@ open class Item(
 open class BaseItem(name: String, sellIn: Int, quality: Int) : Item(name, sellIn, quality) {
     fun update() {
         sellIn -= aging()
-        degrade()
-        saturate()
+        quality = saturation(quality - degradation(sellIn, quality))
     }
 
-    protected open fun degrade() {
-        if (sellIn < 0)
-            quality -= 2
-        else
-            quality -= 1
+    protected open fun degradation(sellIn: Int, quality: Int): Int = when {
+        sellIn < 0 -> 2
+        else -> 1
     }
 
     protected open fun aging() = 1
 
-    protected open fun saturate() {
-        when {
-            quality < 0 -> quality = 0
-            quality > 50 -> quality = 50
-        }
+    protected open fun saturation(quality: Int) = when {
+        quality < 0 -> 0
+        quality > 50 -> 50
+        else -> quality
     }
 }
 
 class Sulfuras(name: String, sellIn: Int, quality: Int) : BaseItem(name, sellIn, quality) {
-    override fun degrade() {}
     override fun aging() = 0
-    override fun saturate() {}
+    override fun degradation(sellIn: Int, quality: Int) = 0
+    override fun saturation(quality: Int) = quality
 }
 
 class Brie(name: String, sellIn: Int, quality: Int) : BaseItem(name, sellIn, quality) {
-    override fun degrade() {
-        if (sellIn < 0)
-            quality += 2
-        else
-            quality += 1
+    override fun degradation(sellIn: Int, quality: Int): Int = when {
+        sellIn < 0 -> -2
+        else -> -1
     }
 }
 
 class Pass(name: String, sellIn: Int, quality: Int) : BaseItem(name, sellIn, quality) {
-    override fun degrade() {
-        quality = when {
-            sellIn < 0 -> 0
-            sellIn < 5 -> quality + 3
-            sellIn < 10 -> quality + 2
-            else -> quality + 1
-        }
+    override fun degradation(sellIn: Int, quality: Int) = when {
+        sellIn < 0 -> quality
+        sellIn < 5 -> -3
+        sellIn < 10 -> -2
+        else -> -1
     }
 }
