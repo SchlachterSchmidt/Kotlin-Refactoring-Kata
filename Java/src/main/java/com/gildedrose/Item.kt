1422469c21b436d618bb09825c9fbf02ae410aed
philipp g <teenageenemanursesinbondage@gmail.com>
2023-04-03 07:33:50 +0200

    make Item data class

--- Java/src/main/java/com/gildedrose/Item.kt
+++ Java/src/main/java/com/gildedrose/Item.kt
@@ -1,30 +1,21 @@
 package com.gildedrose
 
 
-class Item(
-    val name: String,
-    var sellIn: Int = 0,
-    var quality: Int = 0,
-    private val aging:() -> Int = Aging.standard,
+data class Item(
+    private val name: String,
+    private val sellIn: Int = 0,
+    private val quality: Int = 0,
+    private val aging: () -> Int = Aging.standard,
     private val degradation: (Int, Int) -> Int = Degradation.standard,
-    private val saturation: (Int) -> Int = Saturation.standard
+    private val saturation: (Int) -> Int = Saturation.standard,
 ) {
     override fun toString() = "$name, $sellIn, $quality"
-    fun update() {
-        sellIn -= aging()
-        quality = saturation(quality - degradation(sellIn, quality))
-    }
 
     fun updated(): Item {
         val sellIn = sellIn - aging()
-        val quality = saturation(quality - degradation(sellIn, quality))
-        return Item(
-            name,
-            sellIn,
-            quality,
-            aging,
-            degradation,
-            saturation
+        return this.copy(
+            sellIn = sellIn,
+            quality = saturation(this.quality - degradation(sellIn, this.quality))
         )
     }
 }
