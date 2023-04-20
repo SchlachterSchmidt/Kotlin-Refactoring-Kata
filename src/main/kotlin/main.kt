df42de19f1e25a06f4983283b9a51e1b479b8a58
SchlachterSchmidt <teenageenemanursesinbondage@gmail.com>
2023-04-20 18:37:37 +0200

    update stock file when loading items

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -5,6 +5,7 @@ import java.io.File
 fun main() {
     val file = File("stock.tsv").also { it.createNewFile() }
     val stock = file.loadItems()
+    stock.saveTo(file)
 
     val server = Server(routes(stock))
     server.start()
